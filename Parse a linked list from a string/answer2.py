3170229f8289f69466724b4da83943b008250a10
Sofiia Hembara <hembara.pn@ucu.edu.ua>
2024-04-07 17:18:37 +0300

    Fixed some issues

--- Parse a linked list from a string/answer2.py
+++ Parse a linked list from a string/answer2.py
@@ -4,19 +4,18 @@ class Node:
         self.next = next
 
 def linked_list_from_string(s):
-    parts = s.split(' -> ')
-    head = None
-    probe = None
-    for part in reversed(parts):
-        if part == 'None':
-            continue
-        if head is None:
-            head = Node(int(part))
-            probe = head
-        else:
-            new_node = Node(int(part))
-            new_node.next = probe
-            probe = new_node
-    return probe
+    
+    parts = s.split(" -> ")
+    if parts[-1] == "None":
+        parts.pop()
 
-print(linked_list_from_string('0 -> 1 -> 4 -> 9 -> 16 -> None'))
+    if not parts:
+        return None
+
+    head = Node(int(parts[0]))
+    current = head
+    for value in parts[1:]:
+        new_node = Node(int(value))
+        current.next = new_node
+        current = new_node
+    return head
